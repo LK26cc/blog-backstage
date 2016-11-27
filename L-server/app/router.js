ecf58217ccd3b052b6ae9fa2c49bf3ee391b03d3
chaoliu45@creditease.cn <chaoliu45@creditease.cn>
2016-11-27 19:19:00 +0800

    增加登录注册接口

--- L-server/app/router.js
+++ L-server/app/router.js
@@ -1,9 +1,12 @@
 var router = require('koa-router')();
 var pets = require('./pets/pets');
 var user = require('./user/user');
+var bodyParser = require('koa-body')();
 
 router.get('/pets', pets.list);
 router.get('/pets/:name', pets.show);
 router.get('/user',user.list);
-router.get('/user/:id',user.getById);
+router.post('/login',bodyParser,user.login);
+router.post('/register',bodyParser,user.register);
+
 module.exports = router;
--- L-server/app/user/user.js
+++ L-server/app/user/user.js
@@ -32,6 +32,50 @@ var user = {
          data:{}
        };
      }
+  },
+  login:function *(){
+    var name = this.request.body.name,
+        password = this.request.body.password;
+    var row = yield user_service.findOne({
+      'where': {
+        'name': name,
+        'password':password
+      }
+    });
+    if(row){
+       this.body = {
+         status:0,
+         msg:'',
+         data:row.dataValues
+       };
+     }else{//null
+       this.body = {
+         status:0,
+         msg:'没有查到相关数据',
+         data:{}
+       };
+     }
+  },
+  register:function *(){
+    var name = this.request.body.name,
+        password = this.request.body.password;
+    var row = yield user_service.create({
+      'name':name,
+      'password':password
+    });
+    if(row){
+      this.body = {
+        status:0,
+        msg:'操作成功！',
+        data:{}
+      };
+    }else{//null
+       this.body = {
+         status:-1,
+         msg:'操作失败！',
+         data:{}
+       };
+     }
   }
 }
 
