2db3907102ad4015db1f8390205b380089ef1ffe
chaoliu45@creditease.cn <chaoliu45@creditease.cn>
2016-12-07 14:08:05 +0800

    修改webpack配置文件，增加热更替、inline模式

--- L-web/webpack.config.js
+++ L-web/webpack.config.js
@@ -1,21 +1,33 @@
 var webpack = require('webpack');
-
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 module.exports = {
-  entry:['./src/index.js'],
+  devtool: 'eval-source-map',//配置生成Source Maps
+  entry:{
+    index:'./src/index.js'
+  },
   output:{
-    filename: 'index.bundle.js'
+    path:__dirname+'/dist',
+    filename: '[name].bundle.js'//[name]跟entry里面键名有关
   },
   module:{
     loaders:[
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.scss$/, loader: 'style!css!sass?sourceMap'},
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
+      { test: /\.js[x]?$/, loader:'babel-loader',exclude:/node_modules/,query: {presets: ['es2015','react']}}
     ]
   },
   resolve:{
     extensions: ['', '.js', '.jsx', '.scss']
   },
   devServer: {
-    inline:true
-  }
+    inline:true,//inline模式
+    hot: true//热更替
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),//热更替必须加这个插件，否则disable
+    new OpenBrowserPlugin({//自动打开浏览器
+      url: 'http://localhost:8080/index.html'
+    })
+  ]
 };
