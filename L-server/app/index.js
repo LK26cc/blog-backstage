8c98248e0af4a908c8359c0690f23ca847d15b3b
chaoliu45@creditease.cn <chaoliu45@creditease.cn>
2016-12-19 17:26:45 +0800

    解决跨域问题

--- L-server/app/index.js
+++ L-server/app/index.js
@@ -2,7 +2,9 @@ var koa = require('koa');
 var logger = require('koa-logger')
 var app = koa();
 var router = require('./router');
+var cors = require('koa-cors');
 
+app.use(cors());
 app.use(router.routes());
 app.use(logger());//日志
 app.listen(8000);
